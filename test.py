7eb71439f71a491eb117a4ccfca9b03a0bcdf0a5
ChandanaSP <89439504+ChandanaSP@users.noreply.github.com>
2023-02-01 13:04:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
-# testing Fibonacci number function
-def fib(n: int) -> int:
-    return n if n < 2 else fib(n-1)+fib(n-2)
+def add(a, b):
+    return a + b
 
 
-def test_fibonacci():
-    assert fib(10) == 55
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
+
+
+def subtract(a, b):
+    return a + b  # <--- fix this in step 7
+
+
+# uncomment the following test in step 5
+#def test_subtract():
+#    assert subtract(2, 3) == -1
